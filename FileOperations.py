c899d1b56bcec366b8dbfbe37416a2992a09e945
Nipun Chawla <nipun.chawla786@gmail.com>
2019-07-16 22:49:43 -0400

    Read data from files

---
+++ FileOperations.py
@@ -0,0 +1,38 @@
+import logging
+
+def read_users_data(users_file):
+    users = {}
+    with open(users_file, 'r') as r:
+        for line in r.readlines():
+            try:
+                if not str(line).startswith('#'):
+                    user = dict()
+                    line = line.split(':')
+                    user["name"],user['uid'],user["gid"],user["comment"],user["home"],user["shell"]=line[0],  int(line[2]),int(line[3]), line[4], line[5], line[6]
+                    users[user['uid']]=user
+            except ValueError:
+                logging.addLevelName(logging.DEBUG,'')
+                logging.debug('line format not supported')
+        return users
+
+
+def read_group_data(groups_file):
+    groups = {}
+    with open(groups_file, 'r') as r:
+        for line in r.readlines():
+            try:
+                if not str(line).startswith('#') or len(line) == 0:
+                    group = dict()
+                    line  = line.split(':')
+                    group['name'],group['password'],group['gid']= line[0], line[1],int(line[2])
+                    members = []
+                    if len(line[3])>1:
+                        members = line[3].split(',')
+                    group['members'] = members
+                    groups[group['gid']]=group
+
+            except ValueError:
+                logging.addLevelName(logging.DEBUG,'')
+                logging.debug('line format not supported')
+
+        return groups
